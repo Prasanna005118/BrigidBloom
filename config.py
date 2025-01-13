29deb4fdd77328604429f7f730c6814135ae9758
Prasanna1108 <kotaprasanna005@gmail.com>
2025-01-13 13:54:22 +0530

    second change to config

--- config.py
+++ config.py
@@ -2,6 +2,6 @@
 import streamlit as st
 
 # Fetch secrets stored in Streamlit
-OLLAMA_HOST = st.secrets["ollama_host"]
-EMBEDDING_MODEL = st.secrets["embedding_model"]
-LANGUAGE_MODEL = st.secrets["language_model"]
+OLLAMA_HOST = st.secrets["OLLAMA_HOST"]
+EMBEDDING_MODEL = st.secrets["EMBEDDING_MODEL"]
+LANGUAGE_MODEL = st.secrets["LANGUAGE_MODEL"]
