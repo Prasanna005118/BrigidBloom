ae89397064f5c52f72e036afe2a6f242f2dbeb75
Prasanna1108 <kotaprasanna005@gmail.com>
2025-01-13 14:39:15 +0530

    another change

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 OLLAMA_HOST = os.getenv('OLLAMA_HOST', 'http://13.126.129.9:11434')
-EMBEDDING_MODEL = 'CompendiumLabs/bge-base-en-v1.5-gguf'
-LANGUAGE_MODEL = 'bartowski/Llama-3.2-1B-Instruct-GGUF'
+EMBEDDING_MODEL = 'hf.co/CompendiumLabs/bge-base-en-v1.5-gguf'
+LANGUAGE_MODEL = 'hf.co/bartowski/Llama-3.2-1B-Instruct-GGUF'
