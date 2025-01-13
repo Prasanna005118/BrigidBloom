faed3de74daef8a897d81b6d60b618b2034a9b77
Prasanna1108 <kotaprasanna005@gmail.com>
2025-01-13 14:35:16 +0530

    config changed

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
-# config.py
-import streamlit as st
+import os
 
-# Fetch secrets stored in Streamlit
-OLLAMA_HOST = st.secrets["OLLAMA_HOST"]
-EMBEDDING_MODEL = st.secrets["EMBEDDING_MODEL"]
-LANGUAGE_MODEL = st.secrets["LANGUAGE_MODEL"]
+OLLAMA_HOST = os.getenv('OLLAMA_HOST', 'http://13.126.129.9:11434')
+EMBEDDING_MODEL = 'CompendiumLabs/bge-base-en-v1.5-gguf'
+LANGUAGE_MODEL = 'bartowski/Llama-3.2-1B-Instruct-GGUF'
